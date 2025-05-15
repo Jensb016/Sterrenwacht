d12b7b569991bc32322b9b85455cbec1ec88cff4
FrankvanVroenhoven <40201053@roctilburg.nl>
2025-05-15 09:11:59 +0200

    commit

---
+++ Dashboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="Dashboard.php">Dashboard</a></li>
+            <li><a href="DashboardJeugd.php">Dashboard Jeugd</a></li>
+        </ul>
+    </nav>
+    <div class="content">
+    </div>
+</body>
+</html>
+<style>
+    /* background except for nav will be an img */
+    .content {
+        background-image: url('https://sterrenwachthalley.nl/wp-content/uploads/2019/11/halley-background-001.jpg');
+        background-size: cover;
+        background-position: center;
+        font-family: Arial, sans-serif;
+        color: white;
+    }
+</style>
